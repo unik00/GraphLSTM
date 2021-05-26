35396ec9b068758c4158e8faffd720718bfb6398
Ngo Duc Huy <ngoduchuy64@gmail.com>
2021-05-26 15:27:26 +0700

    finished embeddings. TODO: convert shape to [batch_size, di] for all embeddings.

--- graph_lstm.py
+++ graph_lstm.py
@@ -1,11 +1,10 @@
 from typing import List
 
-import numpy as np
 import tensorflow as tf
 from allennlp.modules.elmo import batch_to_ids, Elmo
 from tensorflow.keras.layers import *
 
-from cdr_data import CDRData
+from cdr_data import CDRData, normalize
 from emb_utils import *
 
 
@@ -29,13 +28,40 @@ class CNNCharEmbedding(Layer):
                                      strides=1,
                                      data_format='channels_last')
 
-    def call(self, inputs):
+    def convert_to_ints(self, word):
+        ret = list()
+        word = normalize(word)
+        for c in word:
+            ret.append(self.char_dict[c])
+        return ret
+
+    def pad(self, emb_chars: List[int]):
+        """Center padding one token with 0s
+        Args:
+            emb_chars: embedded characters of one token
+        """
+        front = [0] * ((self.PADDED_LENGTH - len(emb_chars)) // 2)
+        end = [0] * ((self.PADDED_LENGTH - len(emb_chars) + 1) // 2)
+        ret = front + emb_chars + end
+        assert len(ret) == self.PADDED_LENGTH
+        return ret
+
+    def call(self, inputs: List[str]):
         """
         Args:
               inputs: a batch of strings denoting tokens
         """
-        print("inputs: ", inputs)
-        x = self.chars_emb(inputs)
+        input_tensor = list()
+
+        for s in inputs:
+            emb = self.convert_to_ints(s)
+            processed_token = self.pad(emb)
+            processed_token = tf.convert_to_tensor(processed_token)
+            input_tensor.append(processed_token)
+
+        input_tensor = tf.convert_to_tensor(input_tensor)
+
+        x = self.chars_emb(input_tensor)
 
         x = tf.reshape(x, [-1, self.PADDED_LENGTH, self.CHAR_DIM, 1])
         x = self.conv(x)
@@ -77,7 +103,8 @@ class ELMoEmbedding(Layer):
             inputs: a batch of tokenized sentences (strings)
         """
         input_tensor = batch_to_ids(inputs)
-        return self.ELMoEmbedding(input_tensor)
+        # what's the mask is there in the dict for?
+        return tf.convert_to_tensor(self.ELMoEmbedding(input_tensor)["elmo_representations"][0].tolist())
 
 
 class GraphLSTM(tf.keras.Model):
@@ -103,9 +130,10 @@ class GraphLSTM(tf.keras.Model):
         list_POSs = [token.pos_ for token in doc]
 
         # concatenate embeddings
-        es = self.char_emb(tokens)
-        cs = self.elmo_emb([tokens])
         ps = self.pos_emb(list_POSs)
+        es = self.char_emb(tokens)
+        print(tokens)
+        cs = self.elmo_emb(list([tokens]))
         print(es.shape)
         print(cs.shape)
         print(ps.shape)
@@ -115,18 +143,18 @@ class GraphLSTM(tf.keras.Model):
 
 
 if __name__ == "__main__":
+    s = "A bilateral retrobulbar neuropathy with an unusual central bitemporal hemianopic scotoma was found"
+
     data = CDRData()
     char_emb = CNNCharEmbedding(data.char_dict)
-    x = np.random.randint(100, size=(2, 20))
-    print(x)
-    print("char emb x shape: ", char_emb(x).shape)
+    # x = np.random.randint(100, size=(2, 20))
+    # print(x)
+    # print("char emb x shape: ", char_emb(x).shape)
     # elmo_emb = ELMoEmbedding()
-    s = "A bilateral retrobulbar neuropathy with an unusual central bitemporal hemianopic scotoma was found"
     # print(elmo_emb([s.split(" ")]))
-
     # pos_emb = POSEmbedding()
     # print(pos_emb(s).shape)
-    # model = GraphLSTM()
-    # model.emb_single(s)
+    model = GraphLSTM(data)
+    model.emb_single(s)
 
     pass
