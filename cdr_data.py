d762095b28a16293463caedb4192ed69f058db7c
Ngo Duc Huy <ngoduchuy64@gmail.com>
2021-06-03 19:47:58 +0700

    finished build_data_from_file

--- cdr_data.py
+++ cdr_data.py
@@ -22,6 +22,105 @@ def write_dict_down(d, filepath):
             f.write(key + "\n")
 
 
+def build_inter_sentence_docs_from_file(path):
+    """
+    Args:
+        path: a string denoting a path
+
+    Returns:
+        A list of dicts. One dict should contain the following keys:
+            - "doc": spacy docs
+            - "Disease": a dict containing diseases as keys. The value of each key is
+                a list containing one pair of starting end ending token index
+            - "Chemical": the same as "disease"
+            - "relation": a set of pairs (chemical, disease) denoting one CID relation
+    """
+    ret = list()
+
+    with open(path, 'rb') as fp:
+        reader = bioc.BioCXMLDocumentReader(fp)
+
+        for document in reader:
+            current_dict = dict()
+            current_dict['Disease'] = dict()
+            current_dict['Chemical'] = dict()
+
+            for passage in document.passages:
+                assert len(passage.relations) == 0
+                doc = gen_dependency_tree(passage.text)
+                current_dict['doc'] = doc
+
+                for annotation in passage.annotations:
+                    MESH = annotation.infons.get("MESH")
+                    typ = annotation.infons.get("type")
+                    if MESH not in current_dict[typ]:
+                        current_dict[typ][MESH] = list()
+
+                    # convert character location to token location
+                    converted_loc = list()
+                    for location in annotation.locations:
+                        left = -1
+                        right = -1
+
+                        for i, token in enumerate(doc):
+                            if location.offset - passage.offset >= token.idx and \
+                                    location.offset + location.length - passage.offset <= token.idx + len(token):
+                                # In case entity is within token. e.g. "carcinogenic" in "Co-carcinogenic"
+                                left = i
+                                right = i
+                                break
+
+                            if token.idx >= location.offset - passage.offset and left == -1:
+                                left = i
+                            if token.idx < location.offset + location.length - passage.offset:
+                                right = i
+                        converted_loc.append((left, right))
+                    current_dict[typ][MESH] += converted_loc
+
+            current_dict["relation"] = set()
+            for relation in document.relations:
+                assert len(relation.nodes) == 0
+                assert relation.infons.get("relation") == "CID"
+                current_dict["relation"].add((relation.infons.get("Chemical"),
+                                              relation.infons.get("Disease")))
+            ret.append(current_dict)
+
+    return ret
+
+
+def build_intra_sentence_docs_from_file(path):
+    """ Refer to build_intra_sentence_docs_from_file() docstring for arguments explanation """
+    # build inter sentence level first
+    inter_list = build_inter_sentence_docs_from_file(path)
+
+    # convert inter-sentence to intra sentence
+    new_list = list()
+    for old_dict in inter_list:
+        old_doc = old_dict["doc"]
+        new_dict = dict()
+        for sent in old_doc.sents:
+            new_dict["doc"] = sent
+            new_dict["Chemical"] = dict()
+            new_dict["Disease"] = dict()
+            new_dict["relation"] = set()
+
+            for typ in ["Chemical", "Disease"]:
+                for MESH in old_dict[typ]:
+                    for pos in old_dict[typ][MESH]:
+                        if pos[0] >= sent.start and pos[1] <= sent.end:
+                            if MESH not in new_dict[typ]:
+                                new_dict[typ][MESH] = list()
+                            new_dict[typ][MESH].append(pos)
+
+            for relation in old_dict["relation"]:
+                if relation[0] in new_dict["Chemical"] and relation[1] in new_dict["Disease"]:
+                    new_dict["relation"].add(relation)
+
+            print(new_dict)
+            new_list.append(new_dict)
+    return new_list
+
+
 class CDRData:
     DEFAULT_VOCAB_PATH = "saved_datas/train_vocab.txt"
     DEFAULT_CHAR_DICT_PATH = "saved_datas/char_dict.txt"
@@ -78,88 +177,17 @@ class CDRData:
         m = {**m1, **m2}
         return m
 
-    def build_intersentence_docs_from_file(self, path):
+    # TODO: save to file
+    def build_data_from_file(self, path, mode):
         """
         Args:
-            path: a string denoting a path
-
-        Returns:
-            A list of dicts. One dict should contain the following keys:
-                - "doc": spacy docs
-                - "disease": a dict containing diseases as keys. The value of each key is
-                    a list containing one pair of starting end ending token index
-                - "chemical": the same as "disease"
-                - "relation": a set of pairs (chemical, disease) denoting one CID relation
+            path: a string
+            mode: either "inter" or "intra"
         """
-        ret = list()
-
-        with open(path, 'rb') as fp:
-            reader = bioc.BioCXMLDocumentReader(fp)
-
-            for document in reader:
-                current_dict = dict()
-                current_dict['Disease'] = dict()
-                current_dict['Chemical'] = dict()
-
-                for passage in document.passages:
-                    assert len(passage.relations) == 0
-                    doc = gen_dependency_tree(passage.text)
-                    current_dict['doc'] = doc
-
-                    for annotation in passage.annotations:
-                        MESH = annotation.infons.get("MESH")
-                        typ = annotation.infons.get("type")
-                        if MESH not in current_dict[typ]:
-                            current_dict[typ][MESH] = list()
-
-                        # convert character location to token location
-                        converted_loc = list()
-                        for location in annotation.locations:
-                            left = -1
-                            right = -1
-
-                            for i, token in enumerate(doc):
-                                if location.offset - passage.offset >= token.idx and \
-                                        location.offset + location.length - passage.offset <= token.idx + len(token):
-                                    # In case entity is within token. e.g. "carcinogenic" in "Co-carcinogenic"
-                                    left = i
-                                    right = i
-                                    # print("wtf", token)
-                                    # print("ftw", passage.text[location.offset - passage.offset:location.offset + location.length - passage.offset])
-                                    # print(document.id)
-                                    break
-
-                                if token.idx >= location.offset - passage.offset and left == -1:
-                                    left = i
-                                if token.idx < location.offset + location.length - passage.offset:
-                                    right = i
-                            converted_loc.append((left, right))
-                            if doc[left:right + 1].text != annotation.text:
-                                print(doc[left:right + 1].text, annotation.text, left, right)
-                                # print(doc)
-
-                            """
-                            # TODO: need preprocess?
-                            assert doc[left:right + 1].text == annotation.text or \
-                                   doc[left:right + 1].text == annotation.text + "-induced" or \
-                                   doc[left:right + 1].text == "("+annotation.text + ")-induced" or \
-                                   doc[left:right + 1].text == annotation.text + "-related" or \
-                                   doc[left:right + 1].text == annotation.text + "-free" or \
-                                   doc[left:right + 1].text == annotation.text + "-treated" or \
-                                   doc[left:right + 1].text == "Co-" + annotation.text
-                            """
-                        current_dict[typ][MESH] += converted_loc
-
-                current_dict["relation"] = set()
-                for relation in document.relations:
-                    assert len(relation.nodes) == 0
-                    assert relation.infons.get("relation") == "CID"
-                    current_dict["relation"].add((relation.infons.get("Chemical"),
-                                                  relation.infons.get("Disease")))
-                ret.append(current_dict)
-                # print(current_dict)
-
-        return ret
+        if mode == "inter":
+            return build_inter_sentence_docs_from_file(path)
+        elif mode == "intra":
+            return build_intra_sentence_docs_from_file(path)
 
     def __init__(self, build_vocab=False):
         if not path.exists(self.DEFAULT_VOCAB_PATH) \
@@ -176,4 +204,4 @@ if __name__ == "__main__":
     cdr_data = CDRData(build_vocab=False)
     # print(cdr_data.vocab_dict)
     # print(cdr_data.char_dict)
-    cdr_data.build_intersentence_docs_from_file(cdr_data.DEV_DATA_PATH)
+    cdr_data.build_data_from_file(cdr_data.DEV_DATA_PATH, mode="intra")
