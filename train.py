3292d7a59f7299127d82c1afec66a5b9e2abd51b
superkink3124 <ducnhimngn@gmail.com>
2021-11-28 21:21:57 +0700

    Add ner loss function

--- train.py
+++ train.py
@@ -26,6 +26,18 @@ def make_tensor_from_dict(output_dict):
         output.append(output_dict[key])
     return tf.convert_to_tensor(output)
 
+def use_ner_loss_function(x_train, logits, ner_output, weights = [0.5, 0.5]):
+    y_train = make_golden(x_train)
+    
+    re_loss_fn = tf.keras.losses.categorical_crossentropy
+    logits = make_tensor_from_dict(logits)
+    re_loss = tf.math.reduce_sum(loss_fn(y_train, logits))
+    
+    ner_loss_fn = tf.losses.sparse_categorical_crossentropy
+    ner_label = tf.constant(x_train['ner_label'])
+    ner_loss = tf.math.reduce_sum(ner_loss_fn(ner_label, ner_output))
+    
+    return weights[0] * re_loss + weights[1] * ner_loss
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -34,12 +46,12 @@ if __name__ == "__main__":
     parser.add_argument("-b", "--batch_size", help="batch size", type=int, default=1)
     parser.add_argument("-f", '--from_pretrained', dest='from_pretrained', action='store_true')
     parser.set_defaults(from_pretrained=False)
-
+    use_ner = True
     train_args = parser.parse_args()
     print(train_args)
 
     dataset = CDRData()
-    model = GraphLSTM(dataset)
+    model = GraphLSTM(dataset, use_ner=use_ner)
 
     if train_args.from_pretrained:
         model.load_weights("saved_weights/saved")
@@ -61,24 +73,32 @@ if __name__ == "__main__":
         for step, x_train in enumerate(train_data):
             if len(x_train['Chemical']) == 0 or len(x_train['Disease']) == 0:
                 continue
+            if use_ner:
+                with tf.GradientTape() as tape:
+                    logits, ner_output = model(x_train)
+                    loss_value = use_ner_loss_function(x_train, logits, ner_output)
+                grads = tape.gradient(loss_value, model.trainable_weights)
+                # print(model.trainable_weights)
 
-            y_train = make_golden(x_train)
+                optimizer.apply_gradients(zip(grads, model.trainable_weights))
+            else:
+                y_train = make_golden(x_train)
 
-            with tf.GradientTape() as tape:
-                logits = model(x_train)  # Logits for this minibatch
-                logits = make_tensor_from_dict(logits)
+                with tf.GradientTape() as tape:
+                    logits = model(x_train)  # Logits for this minibatch
+                    logits = make_tensor_from_dict(logits)
 
-                # print("logits: ", logits, y_train)
-                loss_value = loss_fn(y_train, logits)
+                    # print("logits: ", logits, y_train)
+                    loss_value = tf.reduce_sum(loss_fn(y_train, logits))
 
-            grads = tape.gradient(loss_value, model.trainable_weights)
-            # print(model.trainable_weights)
+                grads = tape.gradient(loss_value, model.trainable_weights)
+                # print(model.trainable_weights)
 
-            optimizer.apply_gradients(zip(grads, model.trainable_weights))
+                optimizer.apply_gradients(zip(grads, model.trainable_weights))
 
             if step % 1 == 0:
                 print(
-                    "Training loss (for one batch) at step {}: {}".format(step, tf.norm(loss_value))
+                    "Training loss (for one batch) at step {}: {}".format(step, loss_value)
                 )
                 print("Seen so far: %s samples" % ((step + 1) * train_args.batch_size))
 
